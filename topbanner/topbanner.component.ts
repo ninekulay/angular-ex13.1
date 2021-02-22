1e82361acaba9f60c63ebfe7df6c2e5083390431
ninekulay <74440281+ninekulay@users.noreply.github.com>
2021-02-22 16:14:51 +0700

    Add files via upload

---
+++ topbanner/topbanner.component.ts
@@ -0,0 +1,12 @@
+import { Component , Input } from'@angular/core';
+@Component({
+    selector:'app-topbanner',
+    templateUrl:'./topbanner.component.html',
+    styleUrls: ['./topbanner.component.css']
+})
+export class TopBannerComponent {
+    @Input()
+    title: string;
+    description: string;
+    constructor() {}
+}
